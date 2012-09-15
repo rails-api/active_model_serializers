9584b9e147705578f8cb5e6f2a546d0a436c7951
Tee Parham <tee@neighborland.com>
2012-09-15 09:34:11 -0700

    move OrderedSet to separate file

--- lib/active_model/serializer.rb
+++ lib/active_model/serializer.rb
@@ -3,30 +3,6 @@ require "active_support/core_ext/module/anonymous"
 require "set"
 
 module ActiveModel
-  class OrderedSet
-    def initialize(array)
-      @array = array
-      @hash = {}
-
-      array.each do |item|
-        @hash[item] = true
-      end
-    end
-
-    def merge!(other)
-      other.each do |item|
-        next if @hash.key?(item)
-
-        @hash[item] = true
-        @array.push item
-      end
-    end
-
-    def to_a
-      @array
-    end
-  end
-
   # Active Model Serializer
   #
   # Provides a basic serializer implementation that allows you to easily
