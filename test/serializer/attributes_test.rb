2bd231e2e1cf6f8500d6f5de85557135308b9275
Santiago Pastorino <santiago@wyeworks.com>
2013-07-01 14:04:25 -0700

    Serialize just attributes

---
+++ test/serializer/attributes_test.rb
@@ -0,0 +1,44 @@
+require 'newbase/test_helper'
+require 'newbase/active_model/serializer'
+
+module SerializerTest
+  module Attributes
+    class Model
+      def initialize(hash={})
+        @attributes = hash
+      end
+
+      def read_attribute_for_serialization(name)
+        @attributes[name]
+      end
+    end
+
+    class ModelSerializer < ActiveModel::Serializer
+      attributes :attr1, :attr2
+    end
+
+    class Test < ActiveModel::TestCase
+      def setup
+        model = Model.new({ :attr1 => 'value1', :attr2 => 'value2', :attr3 => 'value3' })
+        @model_serializer = ModelSerializer.new(model)
+      end
+
+      def test_attributes_definition
+        assert_equal(['attr1', 'attr2'],
+                     @model_serializer.class._attributes)
+      end
+
+      def test_attributes_serialization_using_serializable_hash
+        assert_equal({
+          'attr1' => 'value1', 'attr2' => 'value2'
+        }, @model_serializer.serializable_hash)
+      end
+
+      def test_attributes_serialization_using_as_json
+        assert_equal({
+          'attr1' => 'value1', 'attr2' => 'value2'
+        }, @model_serializer.as_json)
+      end
+    end
+  end
+end
