93baaa96b19e69098579b51db2f36809cfa28522
Santiago Pastorino <santiago@wyeworks.com>
2013-07-05 14:03:02 -0700

    Implement meta and meta_key for AM::Serializer

---
+++ test/unit/active_model/serializer/meta_test.rb
@@ -0,0 +1,41 @@
+require 'newbase/test_helper'
+require 'newbase/active_model/serializer'
+
+
+module ActiveModel
+  class Serializer
+    class MetaTest < ActiveModel::TestCase
+      def setup
+        @model = Model.new({ :attr1 => 'value1', :attr2 => 'value2', :attr3 => 'value3' })
+      end
+
+      def test_meta
+        model_serializer = ModelSerializer.new(@model, root: 'model', meta: { 'total' => 10 })
+
+        assert_equal({
+          'model' => {
+            'attr1' => 'value1',
+            'attr2' => 'value2'
+          },
+          'meta' => {
+            'total' => 10
+          }
+        }, model_serializer.as_json)
+      end
+
+      def test_meta_using_meta_key
+        model_serializer = ModelSerializer.new(@model, root: 'model', meta_key: :my_meta, my_meta: { 'total' => 10 })
+
+        assert_equal({
+          'model' => {
+            'attr1' => 'value1',
+            'attr2' => 'value2'
+          },
+          'my_meta' => {
+            'total' => 10
+          }
+        }, model_serializer.as_json)
+      end
+    end
+  end
+end
