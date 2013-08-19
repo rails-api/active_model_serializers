f647b7ae74ce898d14ebbc298c01662016380818
Santiago Pastorino <santiago@wyeworks.com>
2013-08-19 17:26:07 -0300

    Implement each_serializer

--- test/unit/active_model/array_serializer_test.rb
+++ test/unit/active_model/array_serializer_test.rb
@@ -27,6 +27,17 @@ module ActiveModel
 
         assert_equal expected, serializer.serializable_array
       end
+
+      def test_array_serializers_each_serializer
+        array = [::Model.new({ name: 'Name 1', description: 'Description 1', comments: 'Comments 1' }),
+                 ::Model.new({ name: 'Name 2', description: 'Description 2', comments: 'Comments 2' })]
+        serializer = ArraySerializer.new(array, each_serializer: ProfileSerializer)
+
+        expected = [{'name' => 'Name 1', 'description' => 'Description 1'},
+                    {'name' => 'Name 2', 'description' => 'Description 2'}]
+
+        assert_equal expected, serializer.serializable_array
+      end
     end
   end
 end
