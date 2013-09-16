14f51f2ea930130ad695eb86c4f2f13512433271
Santiago Pastorino <santiago@wyeworks.com>
2013-09-16 17:11:30 -0300

    Remove everything, rewrite of AMS starts here

--- lib/generators/serializer/templates/serializer.rb
+++
@@ -1,8 +0,0 @@
-<% module_namespacing do -%>
-class <%= class_name %>Serializer < <%= parent_class_name %>
-  attributes <%= attributes_names.map(&:inspect).join(", ") %>
-<% association_names.each do |attribute| -%>
-  has_one :<%= attribute %>
-<% end -%>
-end
-<% end -%>
