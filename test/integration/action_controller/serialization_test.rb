23e6ed32be751f95629203acf4a9283259411698
Santiago Pastorino <santiago@wyeworks.com>
2013-10-09 18:20:17 -0200

    Add default_serializer_options test

--- test/integration/action_controller/serialization_test.rb
+++ test/integration/action_controller/serialization_test.rb
@@ -40,6 +40,36 @@ module ActionController
       end
     end
 
+    class DefaultOptionsForSerializerScopeTest < ActionController::TestCase
+      class MyController < ActionController::Base
+        def default_serializer_options
+          { scope: current_admin }
+        end
+
+        def render_using_scope_set_in_default_serializer_options
+          render json: Profile.new({ name: 'Name 1', description: 'Description 1', comments: 'Comments 1' })
+        end
+
+        private
+
+        def current_user
+          'current_user'
+        end
+
+        def current_admin
+          'current_admin'
+        end
+      end
+
+      tests MyController
+
+      def test_render_using_scope_set_in_default_serializer_options
+        get :render_using_scope_set_in_default_serializer_options
+        assert_equal 'application/json', @response.content_type
+        assert_equal '{"profile":{"name":"Name 1","description":"Description 1 - current_admin"}}', @response.body
+      end
+    end
+
     class ExplicitSerializerScopeTest < ActionController::TestCase
       class MyController < ActionController::Base
         def render_using_implicit_serializer_and_explicit_scope
