73774649e34c5cd4f440df8263f4d8e19895d6f3
Santiago Pastorino <santiago@wyeworks.com>
2013-10-11 14:30:16 -0200

    Implement Generators

---
+++ test/integration/generators/resource_generator_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'rails'
+require 'test_app'
+require 'rails/generators/rails/resource/resource_generator'
+require 'active_model/serializer/generators/resource_override'
+
+class ResourceGeneratorTest < Rails::Generators::TestCase
+  destination File.expand_path('../../../tmp', __FILE__)
+  setup :prepare_destination, :copy_routes
+
+  tests Rails::Generators::ResourceGenerator
+  arguments %w(account)
+
+  def test_serializer_file_is_generated
+    run_generator
+
+    assert_file 'app/serializers/account_serializer.rb', /class AccountSerializer < ActiveModel::Serializer/
+  end
+
+  private
+
+  def copy_routes
+    config_dir = File.join(destination_root, 'config')
+    FileUtils.mkdir_p(config_dir)
+    File.write(File.join(config_dir, 'routes.rb'), 'Rails.application.routes.draw { }')
+  end
+end
