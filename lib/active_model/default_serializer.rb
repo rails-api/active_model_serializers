ef1ec65ae70bdbb5515b50264ba63ca618eecdcf
Santiago Pastorino <santiago@wyeworks.com>
2013-11-01 22:35:01 -0200

    Make DefaultSerializer include AM::Serializable so embedded_in_root_associations is always defined also there

--- lib/active_model/default_serializer.rb
+++ lib/active_model/default_serializer.rb
@@ -1,17 +1,22 @@
+require 'active_model/serializable'
+
 module ActiveModel
   # DefaultSerializer
   #
   # Provides a constant interface for all items
   class DefaultSerializer
+    include ActiveModel::Serializable
+
     attr_reader :object
 
     def initialize(object, options=nil)
       @object = object
     end
 
-    def serializable_hash(*)
+    def as_json(options={})
       @object.as_json
     end
-    alias serializable_object serializable_hash
+    alias serializable_hash as_json
+    alias serializable_object as_json
   end
 end
