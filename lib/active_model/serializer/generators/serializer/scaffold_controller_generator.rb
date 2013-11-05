e0a564aa6876f1e5753af27c6fc0fa7056d35f02
Todd Bealmear <todd@t0dd.io>
2013-11-04 17:45:36 -0800

    Fixed Rails 3 compatibility.

--- lib/active_model/serializer/generators/serializer/scaffold_controller_generator.rb
+++ lib/active_model/serializer/generators/serializer/scaffold_controller_generator.rb
@@ -4,9 +4,11 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 module Rails
   module Generators
     class ScaffoldControllerGenerator
-      source_root File.expand_path('../templates', __FILE__)
+      if Rails::VERSION::MAJOR >= 4
+        source_root File.expand_path('../templates', __FILE__)
 
-      hook_for :serializer, default: true
+        hook_for :serializer, default: true
+      end
     end
   end
 end
