f72115fb7990ef66236e066afaf2dd0cec727ebb
Santiago Pastorino <santiago@wyeworks.com>
2013-12-16 17:45:24 -0200

    Test association inheritance in serializers

---
+++ test/unit/active_model/serializer/associations_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+module ActiveModel
+  class Serializer
+    class AssociationsTest < ActiveModel::TestCase
+      def test_associations_inheritance
+        inherited_serializer_klass = Class.new(PostSerializer) do
+          has_many :users
+        end
+        another_inherited_serializer_klass = Class.new(PostSerializer)
+
+        assert_equal([:comments, :users],
+                     inherited_serializer_klass._associations.keys)
+        assert_equal([:comments],
+                     another_inherited_serializer_klass._associations.keys)
+      end
+    end
+  end
+end
