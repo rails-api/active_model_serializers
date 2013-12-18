62a6b6ffc612be69ae4b6112467c5a8af7eff412
Santiago Pastorino <santiago@wyeworks.com>
2013-12-18 13:04:33 -0200

    Test using a structure that returns a consistent value on Rails 4.0 & 4.1

--- test/unit/active_model/default_serializer_test.rb
+++ test/unit/active_model/default_serializer_test.rb
@@ -4,11 +4,10 @@ module ActiveModel
   class DefaultSerializer
     class Test < ActiveModel::TestCase
       def test_serialize_objects
-        assert_equal(nil, DefaultSerializer.new(nil).serializable_hash)
-        assert_equal(1, DefaultSerializer.new(1).serializable_hash)
-        assert_equal('hi', DefaultSerializer.new('hi').serializable_hash)
-        obj = Struct.new(:a, :b).new(1, 2)
-        assert_equal({ a: 1, b: 2 }, DefaultSerializer.new(obj).serializable_hash)
+        assert_equal(nil, DefaultSerializer.new(nil).serializable_object)
+        assert_equal(1, DefaultSerializer.new(1).serializable_object)
+        assert_equal('hi', DefaultSerializer.new('hi').serializable_object)
+        assert_equal('1..3', DefaultSerializer.new(1..3).serializable_object)
       end
     end
   end
