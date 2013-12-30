3cbcf461a34e80bc171712bedf75940889ea6afb
Adrian Mugnolo <adrian@wyeworks.com>
2013-12-30 14:11:45 -0200

    Set up a logger on tests

--- test/test_app.rb
+++ test/test_app.rb
@@ -3,6 +3,9 @@ class TestApp < Rails::Application
     config.eager_load = false
     config.secret_key_base = 'abc123'
   end
+
+  # Set up a logger to avoid creating a log directory on every run.
+  config.logger = Logger.new(nil)
 end
 
 TestApp.initialize!
