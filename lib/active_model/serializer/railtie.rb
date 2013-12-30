8fa252b4fde07b591990755f10e20c264f5b717a
Santiago Pastorino and Adrian Mugnolo <santiago+adrian@wyeworks.com>
2013-12-30 11:21:01 -0200

    Make sure generator hooks get run
    Use Rails::Engine#load_generators instead of require +
    Rails::Generators::configure!.

--- lib/active_model/serializer/railtie.rb
+++ lib/active_model/serializer/railtie.rb
@@ -1,10 +1,9 @@
 module ActiveModel
   class Railtie < Rails::Railtie
     initializer 'generators' do |app|
-      require 'rails/generators'
+      app.load_generators
       require 'active_model/serializer/generators/serializer/serializer_generator'
       require 'active_model/serializer/generators/serializer/scaffold_controller_generator'
-      Rails::Generators.configure!(app.config.generators)
       require 'active_model/serializer/generators/resource_override'
     end
   end
