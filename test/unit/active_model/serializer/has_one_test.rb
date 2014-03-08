b0d1d06ee17b2b1100cd856350822f29f1ebb825
Ken Ip <ken.ip@pushpulllabs.com>
2014-03-08 14:03:08 -0500

    HasOne nil object should return [] under embedded key.
    Ensure that @wrap_in_array is always respected when set to true even
    when associated object is nil.

--- test/unit/active_model/serializer/has_one_test.rb
+++ test/unit/active_model/serializer/has_one_test.rb
@@ -106,6 +106,21 @@ module ActiveModel
         }, @user_serializer.serializable_hash)
       end
 
+      def test_associations_embedding_nil_ids_including_objects_serialization_using_as_json
+        @association.embed = :ids
+        @association.embed_in_root = true
+        @user.instance_eval do
+          def profile
+            nil
+          end
+        end
+
+        assert_equal({
+          'user' => { name: 'Name 1', email: 'mail@server.com', 'profile_id' => nil },
+          'profiles' => []
+        }, @user_serializer.as_json)
+      end
+
       def test_associations_embedding_ids_including_objects_serialization_using_as_json
         @association.embed = :ids
         @association.embed_in_root = true
