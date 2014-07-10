886ada9151f0bbb07d3da6ec01e590db0bde4455
Steve Klabnik <steve@steveklabnik.com>
2014-07-09 23:05:41 -0400

    don't hold on to serializer
    this is an optimization for now, this may have to change later.

--- lib/active_model/serializer/adapter/null_adapter.rb
+++ lib/active_model/serializer/adapter/null_adapter.rb
@@ -2,12 +2,12 @@ module ActiveModel
   class Serializer
     class Adapter
       class NullAdapter
-        def initialize(serializer)
-          @serializer = serializer
+        def initialize(adapter)
+          @attributes = adapter.attributes 
         end
 
         def to_json(options={})
-          @serializer.attributes.each_with_object({}) do |(attr, value), h|
+          @attributes.each_with_object({}) do |(attr, value), h|
             h[attr] = value
           end.to_json # FIXME: why does passing options here cause {}?
         end
