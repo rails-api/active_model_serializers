87186247484d0c6e6a3a0f1b5ca913cba0d95c39
Thierry Joyal <thierry.joyal@gmail.com>
2014-08-15 18:31:53 +0000

    Fix leaky test global state

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,3 +22,24 @@ ActionController::TestCase.class_eval do
     @routes = TestHelper::Routes
   end
 end
+
+Minitest::Test.class_eval do
+  def before_setup
+    @original_configurations = {}
+
+    ActiveModel::Serializer.setup do |config|
+      config.each do |k, v|
+        @original_configurations[k] = v
+      end
+    end
+  end
+
+  def after_teardown
+    ActiveModel::Serializer.setup do |config|
+      config.clear
+      @original_configurations.each do |k, v|
+        config.send("#{k}=", v)
+      end
+    end
+  end
+end
