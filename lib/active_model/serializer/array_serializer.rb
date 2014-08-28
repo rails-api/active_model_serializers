466c7d5dd863aae294b2c4b4c26ad4f16843485e
Tema Bolshakov <abolshakov@spbtv.com>
2014-08-28 18:46:19 +0400

    Wrap association into Serializers

--- lib/active_model/serializer/array_serializer.rb
+++ lib/active_model/serializer/array_serializer.rb
@@ -1,7 +1,9 @@
 module ActiveModel
   class Serializer
-    class ArraySerializer
-
+    class ArraySerializer < Serializer
+      def initialize(object)
+        @object = object
+      end
     end
   end
 end
