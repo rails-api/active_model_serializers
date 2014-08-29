6bb4501f67fe89cdb27537a71d8623178d9e5b63
Tema Bolshakov <abolshakov@spbtv.com>
2014-08-29 11:37:27 +0400

    JsonApi adapter: serialize association

---
+++ test/adapter/json_api/has_many.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+module ActiveModel
+  class Serializer
+    class Adapter
+      class JsonApiAdapter
+        class HasManyTest < Minitest::Test
+          def setup
+            @post = Post.new(title: 'New Post', body: 'Body')
+            @first_comment = Comment.new(id: 1, body: 'ZOMG A COMMENT')
+            @second_comment = Comment.new(id: 2, body: 'ZOMG ANOTHER COMMENT')
+            @post.comments = [@first_comment, @second_comment]
+            @first_comment.post = @post
+            @second_comment.post = @post
+
+            @serializer = PostSerializer.new(@post)
+            @adapter = ActiveModel::Serializer::Adapter::JsonApiAdapter.new(@serializer)
+          end
+
+          def test_includes_comment_ids
+            assert_equal([1, 2], @adapter.serializable_hash[:links][:comments])
+          end
+
+          def test_includes_linked_comments
+            assert_equal([
+                           {id: 1, body: 'ZOMG A COMMENT'},
+                           {id: 2, body: 'ZOMG ANOTHER COMMENT'}
+                         ], @adapter.serializable_hash[:linked][:comments])
+          end
+        end
+      end
+    end
+  end
+end
