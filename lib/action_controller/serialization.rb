4fe9790bfa242921bd7694588071979cc38162bf
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-10-10 00:52:59 +0900

    Support Rails 4.2.0
    This patch is the same fixes as #655 and #663 .

--- lib/action_controller/serialization.rb
+++ lib/action_controller/serialization.rb
@@ -6,17 +6,19 @@ module ActionController
 
     include ActionController::Renderers
 
-    def _render_option_json(resource, options)
-      serializer = ActiveModel::Serializer.serializer_for(resource)
+    [:_render_option_json, :_render_with_renderer_json].each do |renderer_method|
+      define_method renderer_method do |resource, options|
+        serializer = ActiveModel::Serializer.serializer_for(resource)
 
-      if serializer
-        # omg hax
-        object = serializer.new(resource)
-        adapter = ActiveModel::Serializer.adapter.new(object)
+        if serializer
+          # omg hax
+          object = serializer.new(resource)
+          adapter = ActiveModel::Serializer.adapter.new(object)
 
-        super(adapter, options)
-      else
-        super
+          super(adapter, options)
+        else
+          super(resource, options)
+        end
       end
     end
   end
