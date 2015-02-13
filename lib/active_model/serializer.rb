f2a7bb857c7e90cd6e1fab3b50ccdc9742ea8076
nstruthers <neil.struthers@invisionwfm.com>
2015-02-13 13:39:34 +0000

    Returning DefaultSerializer when we encounter nil, bypassing a costly _const_get search for NilClassSerializer

--- lib/active_model/serializer.rb
+++ lib/active_model/serializer.rb
@@ -56,7 +56,9 @@ end
       attr_reader :key_format
 
       def serializer_for(resource, options = {})
-        if resource.respond_to?(:to_ary)
+        if resource.nil?
+          DefaultSerializer
+        elsif resource.respond_to?(:to_ary)
           if Object.constants.include?(:ArraySerializer)
             ::ArraySerializer
           else
