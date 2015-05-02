6d372cb64c7cf103f217a305d1224538bb505f33
Attila Domokos <adomokos@gmail.com>
2015-05-01 22:01:09 -0500

    Wrapping expected_hash for easier reading

--- test/adapter/json/belongs_to_test.rb
+++ test/adapter/json/belongs_to_test.rb
@@ -32,7 +32,13 @@ module ActiveModel
             serializer = PostSerializer.new(@anonymous_post)
             adapter = ActiveModel::Serializer::Adapter::Json.new(serializer)
 
-            assert_equal({title: "Hello!!", body: "Hello, world!!", id: 43, comments: [], blog: {id: 999, name: "Custom blog"}, author: nil}, adapter.serializable_hash)
+            expected_hash = {title: "Hello!!",
+                             body: "Hello, world!!",
+                             id: 43,
+                             comments: [],
+                             blog: {id: 999, name: "Custom blog"},
+                             author: nil}
+            assert_equal(expected_hash, adapter.serializable_hash)
           end
         end
       end
