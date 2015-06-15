8e1214b4c5ae5c5ae27f0b1531ac5fc6e9d8e256
João Moura <joaomdmoura@gmail.com>
2015-06-15 02:48:39 -0300

    force to use flattenJson when dealing with ArraySerializer

--- lib/active_model/serializer/adapter/json.rb
+++ lib/active_model/serializer/adapter/json.rb
@@ -6,7 +6,7 @@ module ActiveModel
       class Json < Adapter
         def serializable_hash(options = {})
           if serializer.respond_to?(:each)
-            @result = serializer.map{|s| self.class.new(s).serializable_hash }
+            @result = serializer.map{|s| FlattenJson.new(s).serializable_hash }
           else
             @hash = {}
 
