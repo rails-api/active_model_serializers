11ea74b326051dc3a7aabc943dc1f4d614b694e0
João Moura <joaomdmoura@gmail.com>
2015-07-10 11:53:02 -0300

    fixing json adapter fragment cache implementation

--- lib/active_model/serializer/adapter/json/fragment_cache.rb
+++ lib/active_model/serializer/adapter/json/fragment_cache.rb
@@ -5,7 +5,14 @@ module ActiveModel
         class FragmentCache
 
           def fragment_cache(cached_hash, non_cached_hash)
-            non_cached_hash.merge cached_hash
+            core_cached = cached_hash.values.first
+            core_non_cached = non_cached_hash.values.first
+
+            if core_cached.is_a?(Hash) && core_non_cached.is_a?(Hash)
+              core_non_cached.merge core_cached
+            else
+              non_cached_hash.merge cached_hash
+            end
           end
 
         end
