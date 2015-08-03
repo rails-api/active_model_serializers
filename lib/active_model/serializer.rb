ec88329df628f83ccdc01a441930437accbc4089
João M. D. Moura <joao@ride.com>
2015-08-02 23:39:12 -0300

    rebasing with master and fixing conflits to make it work

--- lib/active_model/serializer.rb
+++ lib/active_model/serializer.rb
@@ -118,7 +118,8 @@ module ActiveModel
     end
 
     def self.deserialize(params)
-      permitted_params = adapter.params(@_params, @_associations.keys) || @_params
+      association_keys = _reflections.map(&:name)
+      permitted_params = adapter.params(@_params, association_keys) || @_params
       permitted_key    = adapter.root || root_name
 
       sanitazed_params = params.require(permitted_key.to_sym).permit(permitted_params)
