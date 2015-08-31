8df52d558bcf6d3144606bd6ec2c3858f0144501
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-08-31 15:26:59 -0400

    refactored a bit more, and matched some method names to that of json_api

--- lib/active_model/serializer/adapter/json.rb
+++ lib/active_model/serializer/adapter/json.rb
@@ -11,51 +11,73 @@ module ActiveModel
           else
             @hash = {}
 
-            @core = serialized_attributes_of(serializer, options)
+            @core = resource_object_for(serializer, options)
 
-            serializer.associations.each do |association|
-              serializer = association.serializer
-              opts = association.options
+            add_resource_relationships(serializer)
 
-              if serializer.respond_to?(:each)
-                @hash[association.key] = serialize_array(serializer, opts)
-              else
-                @hash[association.key] = serialized_or_virtual_of(serializer, opts)
-              end
-            end
             @result = @core.merge @hash
           end
 
           { root => @result }
         end
 
+        def fragment_cache(cached_hash, non_cached_hash)
+          Json::FragmentCache.new().fragment_cache(cached_hash, non_cached_hash)
+        end
+
+        private
+
+        # iterate through the associations on the serializer,
+        # adding them to @hash as needed (as singular or plural)
+        def add_resource_relationships(serializer)
+          serializer.associations.each do |association|
+            serializer = association.serializer
+            opts = association.options
+
+            if serializer.respond_to?(:each)
+              add_relationships(association.key, serializer, opts)
+            else
+              add_relationship(association.key, serializer, opts)
+            end
+          end
+
+          @hash
+        end
+
+        # add a singular relationship
+        def add_relationship(key, serializer, options)
+          @hash[key] = serialized_or_virtual_of(serializer, options)
+        end
+
+        # add a many relationship
+        def add_relationships(key, serializer, options)
+          @hash[key] = serialize_array(serializer, options)
+        end
+
         def serialize_array_without_root(serializer, options)
           serializer.map { |s| FlattenJson.new(s).serializable_hash(options) }
         end
 
-        # TODO: what is a virtual value?
+        # a virtual value is something that doesn't need a serializer,
+        # such as a ruby array, or any other raw value
         def serialized_or_virtual_of(serializer, options)
           if serializer && serializer.object
-            serialized_attributes_of(serializer, options)
+            resource_object_for(serializer, options)
           elsif options[:virtual_value]
             options[:virtual_value]
           end
         end
 
-        def serialized_attributes_of(item, options)
+        def serialize_array(serializer, options)
+          serializer.map do |item|
+            resource_object_for(item, options)
+          end
+        end
+
+        def resource_object_for(item, options)
           cache_check(item) do
             item.attributes(options)
           end
-        end
-
-        def serialize_array(serializer, options)
-          serializer.map do |item|
-            serialized_attributes_of(item, options)
-          end
-        end
-
-        def fragment_cache(cached_hash, non_cached_hash)
-          Json::FragmentCache.new().fragment_cache(cached_hash, non_cached_hash)
         end
 
       end
