edfec6c53e11d787faa8b7f7f6c310eca8cac186
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-08-31 12:12:08 -0400

    initial refactor to make way for easy feature adding

--- lib/active_model/serializer/adapter/json.rb
+++ lib/active_model/serializer/adapter/json.rb
@@ -7,40 +7,51 @@ module ActiveModel
         def serializable_hash(options = nil)
           options ||= {}
           if serializer.respond_to?(:each)
-            @result = serializer.map { |s| FlattenJson.new(s).serializable_hash(options) }
+            @result = serialize_array_without_root(serializer, options)
           else
             @hash = {}
 
-            @core = cache_check(serializer) do
-              serializer.attributes(options)
-            end
+            @core = serialized_attributes_of(serializer, options)
 
             serializer.associations.each do |association|
               serializer = association.serializer
               opts = association.options
 
               if serializer.respond_to?(:each)
-                array_serializer = serializer
-                @hash[association.key] = array_serializer.map do |item|
-                  cache_check(item) do
-                    item.attributes(opts)
-                  end
-                end
+                @hash[association.key] = serialize_array(serializer, opts)
               else
-                @hash[association.key] =
-                  if serializer && serializer.object
-                    cache_check(serializer) do
-                      serializer.attributes(options)
-                    end
-                  elsif opts[:virtual_value]
-                    opts[:virtual_value]
-                  end
+                @hash[association.key] = serialized_or_virtual_of(serializer, opts)
               end
             end
             @result = @core.merge @hash
           end
 
           { root => @result }
+        end
+
+        def serialize_array_without_root(serializer, options)
+          serializer.map { |s| FlattenJson.new(s).serializable_hash(options) }
+        end
+
+        # TODO: what is a virtual value?
+        def serialized_or_virtual_of(serializer, options)
+          if serializer && serializer.object
+            serialized_attributes_of(serializer, options)
+          elsif options[:virtual_value]
+            options[:virtual_value]
+          end
+        end
+
+        def serialized_attributes_of(item, options)
+          cache_check(item) do
+            item.attributes(options)
+          end
+        end
+
+        def serialize_array(serializer, options)
+          serializer.map do |item|
+            serialized_attributes_of(item, options)
+          end
         end
 
         def fragment_cache(cached_hash, non_cached_hash)
