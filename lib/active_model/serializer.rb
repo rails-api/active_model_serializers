049b647d3bc05ac38dd5ced9e0d5f298f8677e16
Lucas Hosseini <lucas.hosseini@gmail.com>
2015-09-14 03:54:33 +0200

    Reorganize class methods in Serializer.

--- lib/active_model/serializer.rb
+++ lib/active_model/serializer.rb
@@ -39,69 +39,92 @@ module ActiveModel
       attr_accessor :_cache_except
       attr_accessor :_cache_options
       attr_accessor :_cache_digest
-    end
 
-    def self.inherited(base)
-      base._attributes = _attributes.try(:dup) || []
-      base._attributes_keys = _attributes_keys.try(:dup) || {}
-      base._cache_digest = digest_caller_file(caller.first)
-      super
-    end
-
-    def self.attributes(*attrs)
-      attrs = attrs.first if attrs.first.class == Array
-      @_attributes.concat attrs
-      @_attributes.uniq!
-
-      attrs.each do |attr|
-        define_method attr do
-          object && object.read_attribute_for_serialization(attr)
-        end unless method_defined?(attr) || _fragmented.respond_to?(attr)
+      def inherited(base)
+        base._attributes = _attributes.try(:dup) || []
+        base._attributes_keys = _attributes_keys.try(:dup) || {}
+        base._cache_digest = digest_caller_file(caller.first)
+        super
       end
-    end
 
-    def self.attribute(attr, options = {})
-      key = options.fetch(:key, attr)
-      @_attributes_keys[attr] = { key: key } if key != attr
-      @_attributes << key unless @_attributes.include?(key)
+      def attributes(*attrs)
+        attrs = attrs.first if attrs.first.class == Array
+        @_attributes.concat attrs
+        @_attributes.uniq!
 
-      ActiveModelSerializers.silence_warnings do
-        define_method key do
-          object.read_attribute_for_serialization(attr)
-        end unless (key != :id && method_defined?(key)) || _fragmented.respond_to?(attr)
+        attrs.each do |attr|
+          define_method attr do
+            object && object.read_attribute_for_serialization(attr)
+          end unless method_defined?(attr) || _fragmented.respond_to?(attr)
+        end
       end
-    end
 
-    def self.fragmented(serializer)
-      @_fragmented = serializer
-    end
+      def attribute(attr, options = {})
+        key = options.fetch(:key, attr)
+        @_attributes_keys[attr] = { key: key } if key != attr
+        @_attributes << key unless @_attributes.include?(key)
 
-    # Enables a serializer to be automatically cached
-    def self.cache(options = {})
-      @_cache = ActionController::Base.cache_store if Rails.configuration.action_controller.perform_caching
-      @_cache_key = options.delete(:key)
-      @_cache_only = options.delete(:only)
-      @_cache_except = options.delete(:except)
-      @_cache_options = (options.empty?) ? nil : options
-    end
-
-    def self.serializer_for(resource, options = {})
-      if resource.respond_to?(:serializer_class)
-        resource.serializer_class
-      elsif resource.respond_to?(:to_ary)
-        config.array_serializer
-      else
-        options.fetch(:serializer, get_serializer_for(resource.class))
+        ActiveModelSerializers.silence_warnings do
+          define_method key do
+            object.read_attribute_for_serialization(attr)
+          end unless (key != :id && method_defined?(key)) || _fragmented.respond_to?(attr)
+        end
       end
-    end
 
-    # @see ActiveModel::Serializer::Adapter.lookup
-    def self.adapter
-      ActiveModel::Serializer::Adapter.lookup(config.adapter)
-    end
+      def fragmented(serializer)
+        @_fragmented = serializer
+      end
 
-    def self.root_name
-      name.demodulize.underscore.sub(/_serializer$/, '') if name
+      # Enables a serializer to be automatically cached
+      def cache(options = {})
+        @_cache = ActionController::Base.cache_store if Rails.configuration.action_controller.perform_caching
+        @_cache_key = options.delete(:key)
+        @_cache_only = options.delete(:only)
+        @_cache_except = options.delete(:except)
+        @_cache_options = (options.empty?) ? nil : options
+      end
+
+      def serializer_for(resource, options = {})
+        if resource.respond_to?(:serializer_class)
+          resource.serializer_class
+        elsif resource.respond_to?(:to_ary)
+          config.array_serializer
+        else
+          options.fetch(:serializer, get_serializer_for(resource.class))
+        end
+      end
+
+      # @see ActiveModel::Serializer::Adapter.lookup
+      def adapter
+        ActiveModel::Serializer::Adapter.lookup(config.adapter)
+      end
+
+      def root_name
+        name.demodulize.underscore.sub(/_serializer$/, '') if name
+      end
+
+      def serializers_cache
+        @serializers_cache ||= ThreadSafe::Cache.new
+      end
+
+      def digest_caller_file(caller_line)
+        serializer_file_path = caller_line[CALLER_FILE]
+        serializer_file_contents = IO.read(serializer_file_path)
+        Digest::MD5.hexdigest(serializer_file_contents)
+      end
+
+      def get_serializer_for(klass)
+        serializers_cache.fetch_or_store(klass) do
+          serializer_class_name = "#{klass.name}Serializer"
+          serializer_class = serializer_class_name.safe_constantize
+
+          if serializer_class
+            serializer_class
+          elsif klass.superclass
+            get_serializer_for(klass.superclass)
+          end
+        end
+      end
     end
 
     attr_accessor :object, :root, :meta, :meta_key, :scope
@@ -140,29 +163,6 @@ module ActiveModel
           hash[name] = self.class._fragmented.public_send(name)
         else
           hash[name] = send(name)
-        end
-      end
-    end
-
-    def self.serializers_cache
-      @serializers_cache ||= ThreadSafe::Cache.new
-    end
-
-    def self.digest_caller_file(caller_line)
-      serializer_file_path = caller_line[CALLER_FILE]
-      serializer_file_contents = IO.read(serializer_file_path)
-      Digest::MD5.hexdigest(serializer_file_contents)
-    end
-
-    def self.get_serializer_for(klass)
-      serializers_cache.fetch_or_store(klass) do
-        serializer_class_name = "#{klass.name}Serializer"
-        serializer_class = serializer_class_name.safe_constantize
-
-        if serializer_class
-          serializer_class
-        elsif klass.superclass
-          get_serializer_for(klass.superclass)
         end
       end
     end
