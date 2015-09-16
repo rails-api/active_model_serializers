26277ea1f9581d5a6e84ef324afb7718da2750f0
Benjamin Fleischer <github@benjaminfleischer.com>
2015-09-16 08:49:34 -0500

    Remove duplicate test helper

--- test/support/serialization_testing.rb
+++ test/support/serialization_testing.rb
@@ -1,8 +1,15 @@
-class Minitest::Test
-  def before_setup
-    ActionController::Base.cache_store.clear
+module SerializationTesting
+  private
+
+  def generate_cached_serializer(obj)
+    ActiveModel::SerializableResource.new(obj).to_json
   end
 
+  # Aliased as :with_configured_adapter to clarify that
+  # this method tests the configured adapter.
+  # When not testing configuration, it may be preferable
+  # to pass in the +adapter+ option to <tt>ActiveModel::SerializableResource</tt>.
+  # e.g ActiveModel::SerializableResource.new(resource, adapter: :json_api)
   def with_adapter(adapter)
     old_adapter = ActiveModel::Serializer.config.adapter
     ActiveModel::Serializer.config.adapter = adapter
@@ -10,4 +17,13 @@ class Minitest::Test
   ensure
     ActiveModel::Serializer.config.adapter = old_adapter
   end
+  alias_method :with_configured_adapter, :with_adapter
+end
+
+class Minitest::Test
+  def before_setup
+    ActionController::Base.cache_store.clear
+  end
+
+  include SerializationTesting
 end
