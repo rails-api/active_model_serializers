597ae658c7c7800a66eb53351db2c34f22123fe9
Lucas Hosseini <lucas.hosseini@gmail.com>
2015-09-21 18:22:30 +0200

    Add yarddoc to lookup methods.

--- lib/active_model/serializer/utils.rb
+++ lib/active_model/serializer/utils.rb
@@ -1,6 +1,9 @@
 module ActiveModel::Serializer::Utils
   module_function
 
+  # Gives the existing lookup paths that are suffixes of the fully qualified path of `klass`.
+  # @param [Object] klass
+  # @return [Array<Symbol>]
   def nested_lookup_paths(klass)
     paths = klass.to_s.split('::').reverse.reduce([]) { |a, e| a + [[e] + Array(a.last)] }.reverse
     paths.map! { |path| "#{path.join('::')}" }
@@ -11,6 +14,10 @@ module ActiveModel::Serializer::Utils
     paths
   end
 
+  # Gives the class named `class_name` if it exists (directly) inside one of the lookup `paths`.
+  # @param [Array<Symbol>] paths
+  # @param [Symbol, String] class_name
+  # @return [Object, nil]
   def nested_lookup(paths, class_name)
     class_path = paths.find { |path| path.const_defined?(class_name, false) }
     class_path.const_get(class_name) if class_path
