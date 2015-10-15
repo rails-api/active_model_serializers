0fc543b14d41c1803e08605aa9e32b9b17730f77
Julian Paas <julian.paas@osaaru.com>
2015-10-15 12:08:32 -0400

    Grape formatter feature requested in #1268

---
+++ lib/grape-active_model_serializers.rb
@@ -0,0 +1,6 @@
+# To add graoe support, require 'grape-active_model_serializers' in the base of your grape endpoints
+# Then add 'formatter :json, Grape::Formatters::ActiveModelSerializers' to the endpoints
+# Then add 'helpers Grape::Helpers::ActiveModelSerializers' to the endpoints
+require 'active_model_serializers'
+require 'grape/formatters/active_model_serializers'
+require 'grape/helpers/active_model_serializers'
---
+++ lib/grape/formatters/active_model_serializers.rb
@@ -0,0 +1,17 @@
+# A grape response formatter that can be used as 'formatter :json, Grape::Formatters::ActiveModelSerializers'
+#
+# Serializer options can be passed as a hash from your grape endpoint using env[:active_model_serializer_options],
+# or better yet user the render helper in Grape::Helpers::ActiveModelSerializers
+module Grape
+  module Formatters
+    module ActiveModelSerializers
+      class << self
+        def call(resource, env)
+          serializer_options = {}
+          serializer_options.merge!(env[:active_model_serializer_options]) if env[:active_model_serializer_options]
+          ActiveModel::SerializableResource.new(resource, serializer_options).to_json
+        end
+      end
+    end
+  end
+end
