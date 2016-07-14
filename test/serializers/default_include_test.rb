a9744ea944c91c209591d403998de9c97bbc7f22
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2016-07-14 22:37:57 +0100

    tests refactoring using #serializable tests helper

--- test/serializers/default_include_test.rb
+++ test/serializers/default_include_test.rb
@@ -47,28 +47,28 @@ module ActiveModel
       end
 
       test '#default_include option populate "included" for json_api adapter' do
-        serialized = ActiveModelSerializers::SerializableResource.new(@blog, serializer: BlogSerializer, adapter: :json_api).as_json
+        serialized = serializable(@blog, serializer: BlogSerializer, adapter: :json_api).as_json
 
         assert_equal serialized[:included].size, 3
         assert_equal 'The first post', serialized[:included].first[:attributes][:title]
       end
 
       test '#default_include merges with the render include for json_api adapter' do
-        serialized = ActiveModelSerializers::SerializableResource.new(@blog, serializer: BlogSerializer, adapter: :json_api, include: 'posts.category').as_json
+        serialized = serializable(@blog, serializer: BlogSerializer, adapter: :json_api, include: 'posts.category').as_json
 
         assert_equal serialized[:included].size, 4
         assert_equal 'The first post', serialized[:included].first[:attributes][:title]
       end
 
       test '#default_include option include associations for attributes adapter' do
-        serialized = ActiveModelSerializers::SerializableResource.new(@blog, serializer: BlogSerializer, adapter: :attributes).as_json
+        serialized = serializable(@blog, serializer: BlogSerializer, adapter: :attributes).as_json
 
         assert_equal serialized[:posts].size, 2
         assert_equal 'Blog Author', serialized[:posts].first[:author][:name]
       end
 
       test '#default_include merges with the render include for attributes adapter' do
-        serialized = ActiveModelSerializers::SerializableResource.new(@blog, serializer: BlogSerializer, adapter: :attributes, include: 'posts.category').as_json
+        serialized = serializable(@blog, serializer: BlogSerializer, adapter: :attributes, include: 'posts.category').as_json
 
         assert_equal serialized[:posts].size, 2
         assert_equal 'Blog Author', serialized[:posts].first[:author][:name]
