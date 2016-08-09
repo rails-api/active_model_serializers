6ff9295254235445845de781a314ecb77d7355e2
Benjamin Fleischer <github@benjaminfleischer.com>
2016-08-08 21:03:58 -0500

    No changes necessary, it appears

--- lib/active_model/serializer/fieldset.rb
+++ lib/active_model/serializer/fieldset.rb
@@ -20,21 +20,11 @@ module ActiveModel
       private
 
       def parsed_fields
-        # TODO: this is not very flexible.
-        # - we should probably support symbol, array, hash, etc
-        #
-        # JSONAPI::IncludeDirective supports these, but also changes the resulting structure.
-        # is it worth it to duplicate some of that functionality?
-        # or should IncludeDirective be modified to only allow one layer of options?
         if raw_fields.is_a?(Hash)
-          symbolize_hash(raw_fields)
+          raw_fields.each_with_object({}) { |(k, v), h| h[k.to_sym] = v.map(&:to_sym) }
         else
           {}
         end
-      end
-
-      def symbolize_hash(hash)
-        hash.each_with_object({}) { |(k, v), h| h[k.to_sym] = v.map(&:to_sym) }
       end
     end
   end
