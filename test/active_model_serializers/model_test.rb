1896e5a525cf19d728f3d3cfa9a361d86cc481c3
Yevhen Shemet <yevhene@gmail.com>
2016-08-16 13:49:10 +0300

    ActiveModelSerializers::Model successor initialized with string keys fix (#1881)

--- test/active_model_serializers/model_test.rb
+++ test/active_model_serializers/model_test.rb
@@ -7,5 +7,16 @@ module ActiveModelSerializers
     def setup
       @resource = ActiveModelSerializers::Model.new
     end
+
+    def test_initialization_with_string_keys
+      klass = Class.new(ActiveModelSerializers::Model) do
+        attr_accessor :key
+      end
+      value = 'value'
+
+      model_instance = klass.new('key' => value)
+
+      assert_equal model_instance.read_attribute_for_serialization(:key), value
+    end
   end
 end
