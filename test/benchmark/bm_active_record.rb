9d7957fd5fe0505dde9934df119db72c51331a4d
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-24 16:22:39 -0400

    fix spacing

--- test/benchmark/bm_active_record.rb
+++ test/benchmark/bm_active_record.rb
@@ -18,8 +18,8 @@ require 'sqlite3'
 
 # Change the following to reflect your database settings
 ActiveRecord::Base.establish_connection(
-  adapter:  'sqlite3',
-  database:     ':memory:'
+  adapter: 'sqlite3',
+  database: ':memory:'
 )
 
 # Don't show migration output when constructing fake db
