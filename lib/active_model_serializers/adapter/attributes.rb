a7a8d9e09f32b447df09dd4566067c4b9baed380
Konstantin Kosmatov <key@kosmatov.ru>
2017-04-05 12:42:10 +0000

    Add fields support to JSON adapter

--- lib/active_model_serializers/adapter/attributes.rb
+++ lib/active_model_serializers/adapter/attributes.rb
@@ -2,7 +2,7 @@ module ActiveModelSerializers
   module Adapter
     class Attributes < Base
       def serializable_hash(options = nil)
-        options = serialization_options(options)
+        options ||= serialization_options(options)
         options[:fields] ||= instance_options[:fields]
         serialized_hash = serializer.serializable_hash(instance_options, options, self)
 
---
+++ test/adapter/json/fields_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+module ActiveModelSerializers
+  module Adapter
+    class Json
+      class FieldsTest < ActiveSupport::TestCase
+        class Post < ::Model
+          attributes :title, :body
+          associations :author, :comments
+        end
+        class Author < ::Model
+          attributes :name, :birthday
+        end
+        class Comment < ::Model
+          attributes :body
+          associations :author, :post
+        end
+
+        class PostSerializer < ActiveModel::Serializer
+          type 'posts'
+          attributes :title, :body
+          belongs_to :author
+          has_many :comments
+        end
+
+        class AuthorSerializer < ActiveModel::Serializer
+          attributes :name, :birthday
+        end
+
+        class CommentSerializer < ActiveModel::Serializer
+          type 'comments'
+          attributes :body
+          belongs_to :author
+        end
+
+        def setup
+          ActiveModelSerializers.config.default_includes = ''
+          @author = Author.new(id: 1, name: 'Lucas', birthday: '10.01.1990')
+          @comment1 = Comment.new(id: 7, body: 'cool', author: @author)
+          @comment2 = Comment.new(id: 12, body: 'awesome', author: @author)
+          @post = Post.new(id: 1337, title: 'Title 1', body: 'Body 1',
+                           author: @author, comments: [@comment1, @comment2])
+          @comment1.post = @post
+          @comment2.post = @post
+        end
+
+        def test_fields_attributes
+          fields = { posts: [:title] }
+          hash = serializable(@post, adapter: :json, fields: fields).serializable_hash
+          expected = { title: 'Title 1' }
+          assert_equal(expected, hash[:posts])
+        end
+
+        def test_fields_included
+          fields = { posts: [:title], comments: [:body] }
+          hash = serializable(@post, adapter: :json, include: [:comments], fields: fields).serializable_hash
+          expected = [{ body: @comment1.body }, { body: @comment2.body }]
+
+          assert_equal(expected, hash[:posts][:comments])
+        end
+      end
+    end
+  end
+end
