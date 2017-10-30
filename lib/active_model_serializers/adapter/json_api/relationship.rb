cf29db34c64ee7bd7ca73b97b23169fcac9c8dc8
Benjamin Fleischer <github@benjaminfleischer.com>
2017-10-29 19:40:46 -0500

    Fix JSON:API polymorphic type regression from v0.10.5

--- lib/active_model_serializers/adapter/json_api/relationship.rb
+++ lib/active_model_serializers/adapter/json_api/relationship.rb
@@ -46,7 +46,14 @@ module ActiveModelSerializers
           if association.belongs_to? &&
               parent_serializer.object.respond_to?(association.reflection.foreign_key)
             id = parent_serializer.read_attribute_for_serialization(association.reflection.foreign_key)
-            type = association.reflection.type.to_s
+            if association.polymorphic?
+              # We can't infer resource type for polymorphic relationships from the serializer.
+              # We can ONLY know a polymorphic resource type by inspecting each resource.
+              serializer = association.lazy_association.serializer
+              type = serializer.json_key
+            else
+              type = association.reflection.type.to_s
+            end
             ResourceIdentifier.for_type_with_id(type, id, serializable_resource_options)
           else
             # TODO(BF): Process relationship without evaluating lazy_association
