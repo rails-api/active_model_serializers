a0de45a4d8ca2727b148a6c5456bf6db80152f6d
Benjamin Fleischer <github@benjaminfleischer.com>
2017-10-29 19:40:04 -0500

    Fix JSON:API: for_type_and_id should always inflect_type
    Should Serializer._type ever be inflected?
    Right now, it won't be, but association.serializer._type will be inflected.
    
    That's the current behavior.

--- lib/active_model_serializers/adapter/json_api/resource_identifier.rb
+++ lib/active_model_serializers/adapter/json_api/resource_identifier.rb
@@ -9,6 +9,7 @@ module ActiveModelSerializers
 
         def self.for_type_with_id(type, id, options)
           return nil if id.blank?
+          type = inflect_type(type)
           {
             id: id.to_s,
             type: type_for(:no_class_needed, type, options)
@@ -17,13 +18,17 @@ module ActiveModelSerializers
 
         def self.raw_type_from_serializer_object(object)
           class_name = object.class.name # should use model_name
-          serializer_type = class_name.underscore
+          raw_type = class_name.underscore
+          raw_type = inflect_type(raw_type)
+          raw_type
+            .gsub!('/'.freeze, ActiveModelSerializers.config.jsonapi_namespace_separator)
+          raw_type
+        end
+
+        def self.inflect_type(type)
           singularize = ActiveModelSerializers.config.jsonapi_resource_type == :singular
           inflection = singularize ? :singularize : :pluralize
-          serializer_type = ActiveSupport::Inflector.public_send(inflection, serializer_type)
-          serializer_type
-            .gsub!('/'.freeze, ActiveModelSerializers.config.jsonapi_namespace_separator)
-          serializer_type
+          ActiveSupport::Inflector.public_send(inflection, type)
         end
 
         # {http://jsonapi.org/format/#document-resource-identifier-objects Resource Identifier Objects}
@@ -44,7 +49,8 @@ module ActiveModelSerializers
         private
 
         def type_for(serializer, transform_options)
-          self.class.type_for(serializer, serializer._type, transform_options)
+          serializer_type = serializer._type
+          self.class.type_for(serializer, serializer_type, transform_options)
         end
 
         def id_for(serializer)
