2581fe03623d4ee68093aac0300d62f4bd466ebb
Wasif Hossain <wasifhossain@users.noreply.github.com>
2019-07-17 15:51:15 +0600

    Lookup fieldset using either string or symbol

--- test/serializers/fieldset_test.rb
+++ test/serializers/fieldset_test.rb
@@ -5,11 +5,21 @@ require 'test_helper'
 module ActiveModel
   class Serializer
     class FieldsetTest < ActiveSupport::TestCase
+      def setup
+        @fieldset = ActiveModel::Serializer::Fieldset.new('post' => %w(id title), 'comment' => ['body'])
+      end
+
       def test_fieldset_with_hash
-        fieldset = ActiveModel::Serializer::Fieldset.new('post' => %w(id title), 'comment' => ['body'])
         expected = { post: [:id, :title], comment: [:body] }
 
-        assert_equal(expected, fieldset.fields)
+        assert_equal(expected, @fieldset.fields)
+      end
+
+      def test_fields_for_accepts_string_or_symbol
+        expected = [:id, :title]
+
+        assert_equal(expected, @fieldset.fields_for(:post))
+        assert_equal(expected, @fieldset.fields_for('post'))
       end
     end
   end
